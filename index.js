fbbdc57e3db07ac275ab12b82883aebcb9e879ed
Asa <coreyAsa@outlook.com>
2023-07-10 21:34:47 +0200

    update

--- index.js
+++ index.js
@@ -18,13 +18,20 @@ const wss = new WebSocket.Server({ server });
 // Handle WebSocket connections
 wss.on('connection', ws => {
   // Send the current grid mapping to the new client
-  ws.send(JSON.stringify(gridMapping));
+  ws.send(JSON.stringify({
+    type: 'grid',
+    msg: gridMapping
+  }));
+  ws.send(JSON.stringify({
+    type: 'msg',
+    msg: msgs
+  }))
   guests.push(ws);
 
   // Handle incoming messages from clients
   ws.on('message', message => {
     const data_obj = JSON.parse(message);
-    if(data_obj?.type === "msg1"){
+    if (data_obj?.type === "msg") {
 
       console.log("here: ", data_obj);
       //messages stack
@@ -37,7 +44,7 @@ wss.on('connection', ws => {
 
       const data = {
         type: 'msg',
-        data: msgs
+        msg: msgs
       }
       wss.clients.forEach(client => {
         if (client.readyState === WebSocket.OPEN) {
@@ -45,36 +52,42 @@ wss.on('connection', ws => {
         }
       });
     }
-    else{
+    if (data_obj?.type === 'grid') {
       // Update the grid mapping with the received data
-      gridMapping = data_obj;
-      history.push(data_obj);
-  
+      gridMapping = data_obj.msg;
+      history.push(data_obj.msg);
+
       // Broadcast the updated mapping to all connected clients
       wss.clients.forEach(client => {
         if (client.readyState === WebSocket.OPEN) {
-          client.send(JSON.stringify(gridMapping));
+          client.send(JSON.stringify({
+            type: 'grid',
+            msg: gridMapping
+          }));
         }
       });
     }
-    
-    
+
+
   });
 });
 
 app.get("/undo", (req, res) => {
-    if(history.length > 0){
-        history.pop();
-        const result = history[history.length - 1] || {};
-        console.log("here: ", result);
-        wss.clients.forEach(client => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(JSON.stringify(result));
-            }
-        });
-        return res.json("success");
-    }
-    return res.json("nothing to undo");
+  if (history.length > 0) {
+    history.pop();
+    const result = history[history.length - 1] || {};
+    console.log("here: ", result);
+    wss.clients.forEach(client => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify({
+          type: 'grid',
+          msg: result
+        }));
+      }
+    });
+    return res.json("success");
+  }
+  return res.json("nothing to undo");
 });
 
 // app.get('/*', (req, res) => {
--- public/index.js
+++ public/index.js
@@ -1,96 +1,100 @@
 const socket = new WebSocket('ws://localhost:3000');
 
-    // Grid dimensions
-    const rows = 5;
-    const cols = 8;
+// Grid dimensions
+const rows = 5;
+const cols = 8;
 
-    // Store the current grid mapping
-    let gridMapping = {};
+// Store the current grid mapping
+let gridMapping = {};
 
-    // Create the grid
-    const gridContainer = $('#grid');
-    const messageContainer = $('#msgs');
-    for (let i = 0; i < rows; i++) {
-      for (let j = 0; j < cols; j++) {
-        const cell = $('<div class="cell green"></div>');
-        cell.click(() => {
-          toggleCellColor(i, j);
-        });
-        gridContainer.append(cell);
-      }
-      gridContainer.append('<br>');
+// Create the grid
+const gridContainer = $('#grid');
+const messageContainer = $('#msgs');
+for (let i = 0; i < rows; i++) {
+  for (let j = 0; j < cols; j++) {
+    const cell = $('<div class="cell green"></div>');
+    cell.click(() => {
+      toggleCellColor(i, j);
+    });
+    gridContainer.append(cell);
+  }
+  gridContainer.append('<br>');
+}
+
+// Toggle cell color and send update to the server
+function toggleCellColor(row, col) {
+  const cell = gridContainer.children('div').eq(row * cols + col);
+  const isRed = cell.hasClass('red');
+
+  if (isRed) {
+    cell.removeClass('red').addClass('green');
+  } else {
+    cell.removeClass('green').addClass('red');
+  }
+
+  gridMapping[`${row},${col}`] = !isRed;
+  const data = {
+    type: 'grid',
+    msg: gridMapping
+  }
+  socket.send(JSON.stringify(data));
+}
+
+// Handle grid updates from the server
+socket.addEventListener('message', event => {
+  const data = JSON.parse(event.data);
+  if (data?.type === "msg") {
+    //msg data
+    const messageData = data.msg;
+    messageContainer.empty();
+
+    for (let i = 0; i < messageData.length; i++) {
+      const msg = messageData[i];
+      const p = $(`<p>guest${msg.guestIndex}: ${msg.msg}</p>`);
+      messageContainer.append(p);
     }
 
-    // Toggle cell color and send update to the server
-    function toggleCellColor(row, col) {
-      const cell = gridContainer.children('div').eq(row * cols + col);
-      const isRed = cell.hasClass('red');
+  }
+  if (data?.type === "grid") {
+    //grid data
+    const newGridMapping = data.msg;
+    console.log("here: ", newGridMapping);
 
-      if (isRed) {
-        cell.removeClass('red').addClass('green');
-      } else {
+    // Update the local grid mapping
+    gridMapping = newGridMapping;
+
+    // Update the grid based on the new mapping
+    gridContainer.children('div').each(function (index) {
+      const row = Math.floor(index / cols);
+      const col = index % cols;
+      const cell = $(this);
+
+      if (gridMapping[`${row},${col}`]) {
         cell.removeClass('green').addClass('red');
+      } else {
+        cell.removeClass('red').addClass('green');
       }
-
-      gridMapping[`${row},${col}`] = !isRed;
-      socket.send(JSON.stringify(gridMapping));
-    }
-
-    // Handle grid updates from the server
-    socket.addEventListener('message', event => {
-      const data= JSON.parse(event.data);
-      if(data?.type === "msg"){
-        //msg data
-        const messageData = data.data;
-        messageContainer.empty();
-
-        for(let i = 0; i < messageData.length; i++){
-          const msg = messageData[i];
-          const p = $(`<p>guest${msg.guestIndex}: ${msg.msg}</p>`);
-          messageContainer.append(p);
-        }
-
-      }
-      else{
-        //grid data
-        const newGridMapping = data;
-        console.log("here: ", newGridMapping);
-  
-        // Update the local grid mapping
-        gridMapping = newGridMapping;
-  
-        // Update the grid based on the new mapping
-        gridContainer.children('div').each(function (index) {
-          const row = Math.floor(index / cols);
-          const col = index % cols;
-          const cell = $(this);
-  
-          if (gridMapping[`${row},${col}`]) {
-            cell.removeClass('green').addClass('red');
-          } else {
-            cell.removeClass('red').addClass('green');
-          }
-        });
-      }   
     });
+  }
+});
 
-    // Undo button functionality
-    $('#undoBtn').click(() => {
-        $.get("http://localhost:3000/undo").then(data => {
-            console.log("undo result: ", data);
-        })
-        .fail(() => {
-            alert("undo failed");
-        });
+// Undo button functionality
+$('#undoBtn').click(() => {
+  $.get("http://localhost:3000/undo").then(data => {
+    console.log("undo result: ", data);
+  })
+    .fail(() => {
+      alert("undo failed");
     });
+});
 
-    $('#sendMsg').click(() => {
-      const text = $('#msg').val();
-      // console.log("message: ", text);
-      const data = { 
-        type: 'msg1',
-        msg: text
-      }
-      socket.send(JSON.stringify(data));
-    })
+$('#sendMsg').click(() => {
+  const text = $('#msg').val();
+  // console.log("message: ", text);
+  const data = {
+    type: 'msg',
+    msg: text
+  }
+  socket.send(JSON.stringify(data));
+})
 
